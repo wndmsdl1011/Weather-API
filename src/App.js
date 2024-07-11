e979249783734ee84fca253487a459664bc0d714
wndmsdl1011 <jueun7547@naver.com>
2024-07-11 20:45:54 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -3,41 +3,61 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
-  
-  const [weather,setWeather]=useState.apply(null);
-  const cities=['Paris','New York','Tokyo',"Seoul"]
+
+  const [weather, setWeather] = useState.apply(null);
+  const cities = ['Paris', 'New York', 'Tokyo', "Seoul"]
+  const [city, setCity] = useState('')
+  const [loading, setLoading] = useState(false)
   const getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition((position)=>{
+    navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude
       let lon = position.coords.longitude
-      getWeatherByCurrentLocation(lat,lon)
+      getWeatherByCurrentLocation(lat, lon)
     })
   }
 
-  const getWeatherByCurrentLocation = async(lat,lon) => {
-    let url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=06c86b306be5bf0a4796dc774a9fc0d1&units=metric`
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=06c86b306be5bf0a4796dc774a9fc0d1&units=metric`
+    setLoading(true)
     let response = await fetch(url)
     let data = await response.json();
-    console.log("data",data)
     setWeather(data);
+    setLoading(false)
   }
 
-  useEffect(()=>{
-    getCurrentLocation()
-  },[])
+  const getWeatherByCity = async () => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06c86b306be5bf0a4796dc774a9fc0d1&units=metric`
+    setLoading(true)
+    let response = await fetch(url)
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if (city == "") {
+      getCurrentLocation()
+    } else {
+      getWeatherByCity()
+    }
+  }, [city])
 
 
   return (
     <div>
-      <div className='container'>
-        <WeatherBox weather={weather}/>
-        <WeatherButton cities={cities}/>
-      </div>
-      
+      {loading ? (
+        <div className='container'>
+        <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+        ) : ( <div className='container'>
+        <WeatherBox weather={weather} />
+        <WeatherButton cities={cities} setCity={setCity} />
+      </div>) }
     </div>
   );
-};  
+};
 
 export default App;
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const WeatherBox = ({weather}) => {
+const WeatherBox = ({ weather }) => {
+  const celsius = Math.round(weather?.main.temp);
+  const fahrenheit = Math.round((weather?.main.temp * 9/5) + 32);
+
   return (
     <div className='weather-box'>
       <div>{weather?.name}</div>
-      <h2>{weather?.main.temp}°C / {(((weather?.main.temp)*9/5)+32)}°F</h2>
+      <h2>{celsius}°C / {fahrenheit}°F</h2>
       <h3>{weather?.weather[0].description}</h3>
     </div>
   )
